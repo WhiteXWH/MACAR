e18494a726821701e01fe0d36ca8f48554189f59
WhiteXWH <91547554+WhiteXWH@users.noreply.github.com>
2024-10-19 16:51:36 +0800

    commit

--- MACAR_calculate.py
+++ MACAR_calculate.py
@@ -11,27 +11,24 @@ if __name__ == '__main__':
 
     args = parser.parse_args()
     tool = dataProcess.Tool()
+    tool.make_multilevel_directory(args.output_dir)
 
-    if not os.path.exists(args.output_dir):
-        os.mkdir(args.output_dir)
+    dataset = tool.read_json(args.answer_dir)
+    dataset_length = len(dataset)
 
-    for file in os.listdir(args.answer_dir):
-        dataset = tool.read_json(f"{args.answer_dir}/{file}")
-        dataset_length = len(dataset)
+    if "APIBENCH-Q" in args.answer_dir:
+        dataset_type = "APIBENCH-Q"
+    else:
+        dataset_type = "New-Benchmark"
 
-        if "APIBENCH-Q" in file:
-            dataset_type = "APIBENCH-Q"
-        else:
-            dataset_type = "New-Benchmark"
+    gpt_type = args.answer_dir.split("-", 1)[-1].split(")", 1)[0]
+    pre_file_str = args.answer_dir.rsplit("/", 1)[-1].split(".json")[0]
+    fail_samples_path = f"{args.output_dir}/{pre_file_str}-fail_samples.json"
 
-        gpt_type = file.split("-", 1)[-1].split(")", 1)[0]
-        pre_file_str = file.split(".json")[0]
-        fail_samples_path = f"{args.output_dir}/{pre_file_str}-fail_samples.json"
+    macar_result = tool.calculate(dataset, "FinalAnswer", dataset.keys())
+    gpt_result = tool.calculate(dataset, "RecommendationList1",  dataset.keys(), fail_samples_path)
 
-        macar_result = tool.calculate(dataset, "FinalAnswer", dataset.keys())
-        gpt_result = tool.calculate(dataset, "RecommendationList1",  dataset.keys(), fail_samples_path)
-
-        print("-" * 20 + f"GPT-{gpt_type}-{dataset_type}" + "-" * 20)
-        tool.print_result(gpt_result, dataset_length)
-        print("-" * 20 + f"MACAR(GPT-{gpt_type})-{dataset_type}" + "-" * 20)
-        tool.print_result(macar_result, dataset_length)
+    print("-" * 20 + f"GPT-{gpt_type}-{dataset_type}" + "-" * 20)
+    tool.print_result(gpt_result, dataset_length)
+    print("-" * 20 + f"MACAR(GPT-{gpt_type})-{dataset_type}" + "-" * 20)
+    tool.print_result(macar_result, dataset_length)
