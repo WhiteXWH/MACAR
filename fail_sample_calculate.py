48697621a651fc181307ee7456892ea84a3c356f
WhiteXWH <91547554+WhiteXWH@users.noreply.github.com>
2024-10-19 16:50:49 +0800

    commit

--- fail_sample_calculate.py
+++ fail_sample_calculate.py
@@ -3,17 +3,18 @@ import argparse
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
-    parser.add_argument("--dataset", type=str, required=True,
-                        help="dataset type")
-
+    parser.add_argument("--fail_dir", type=str, required=True,
+                        help="fail samples file path")
+    parser.add_argument("--macar_dir", type=str, required=True,
+                        help="fail samples file path")
 
     args = parser.parse_args()
     tool = dataProcess.Tool()
 
-    fail_samples = tool.read_json(f"result/MACAR/MACAR(GPT-3.5)-{args.dataset}-fail_samples.json")
+    fail_samples = tool.read_json(args.fail_dir)
     fail_samples_length = len(fail_samples)
-    macar_gpt4 = tool.read_json(f"result/MACAR/MACAR(GPT-4)-{args.dataset}-fail_samples.json")
     fail_samples_number_set = fail_samples.keys()
+    macar_gpt4 = tool.read_json(args.macar_dir)
 
     gpt4_result = tool.calculate(macar_gpt4, "RecommendationList1", fail_samples_number_set)
     macar_gpt35_result = tool.calculate(fail_samples, "FinalAnswer", fail_samples_number_set)
